2be261fbba31108f7e16a8da9a8dc23b91ba8bf4
Dvir David Biton <dbiton@gmail.com>
2025-02-12 23:22:30 +0200

    Add OPT test

--- test_cache_policy.py
+++ test_cache_policy.py
@@ -1,38 +1,33 @@
 import numpy as np
 import pytest
+import faiss
 
-from cache_policy import OPT
+from cache import OPT, LFU
 
 def test_opt_two_clusters():
-    count_region1 = 100
-    count_region2 = 200
+    count_region1 = 256
+    count_region2 = 256
     region1 = np.random.rand(count_region1, 2)
     region2 = np.random.rand(count_region2, 2)
     region2[:, 0] += 10
     embeds = np.vstack([region1, region2])
     np.random.shuffle(embeds)
-
-    same_embed_distance = 2
+    same_embed_distance = 0.5
     
-    p = OPT(embeds, same_embed_distance)
-    p.set_size(2)
-    cache = {}
-    cache_hits = 0
-    for i_embed, embed in enumerate(embeds):
-        if len(cache) > 0:
-            stored_embeds = np.array(list(cache.values()))
-            distances = np.linalg.norm(stored_embeds - embed, axis=1)
-            if distances.min() < same_embed_distance:
-                cache_hits += 1
-        id_remove, add_id = p.log_access(i_embed, embed, None)
-        if id_remove != -1:
-            print("pop", id_remove, embeds[id_remove])
-            cache.pop(id_remove)
-        if add_id:
-            print("push", i_embed, embed)
-            cache[i_embed] = embed
-    expected_cache_hits = count_region1 + count_region2 - 2
-    assert expected_cache_hits == cache_hits
+    lfu = LFU(same_embed_distance)
+    opt = OPT(same_embed_distance, embeds)
+    
+    index_lfu = faiss.IndexIDMap2(faiss.IndexFlatL2(2))
+    index_opt = faiss.IndexIDMap2(faiss.IndexFlatL2(2))
+    opt.initialize(32, index_opt)
+    lfu.initialize(32, index_lfu)
+    
+    opt_cache_hits = 0
+    lfu_cache_hits = 0
+    for embed_id, embed in enumerate(embeds):
+        opt_cache_hits += np.sum(opt.request(embed.reshape(1, -1), [embed_id])[0])
+        lfu_cache_hits += np.sum(lfu.request(embed.reshape(1, -1), [embed_id])[0])
+    assert lfu_cache_hits < opt_cache_hits
     
 if __name__ == "__main__":
     pytest.main()
